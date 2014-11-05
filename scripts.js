9cbcbd995993ef11627d46eca29c85bc015d6567
Zekt <asfgjk123123@gmail.com>
2014-11-05 17:45:23 +0800

    section 1&2 almost complete

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
 $(function(){
-	$(".parallax").scroll(function(){
-		var st = $(this).scrollTop();
-		if (st > lastScrollTop){
-			$(".parallax").animate({
-				scrollTop: $("#thebike").offset().top
-			}, 1000);
-		} else {
-		}
+	$("#nav_sec1").click(function(){
+			$('html,body').animate({
+				scrollTop: $('#sec1').offset().top
+			}, 700);
+	});
+	$("#nav_sec2").click(function(){
+			$('html,body').animate({
+				scrollTop: $('#sec2').offset().top
+			}, 700);
+	});
+	$("#nav_sec3").click(function(){
+			$('html,body').animate({
+				scrollTop: $('#sec3').offset().top
+			}, 700);
 	});
 });
